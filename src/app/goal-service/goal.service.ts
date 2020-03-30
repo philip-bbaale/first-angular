e7ee89a7b209654b501f56122b367048f2da458e
Pip Bbaale <namugera.philip@gmail.com>
2020-03-30 18:00:31 +0300

    ...

---
+++ src/app/goal-service/goal.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Goal } from '../goal'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoalService {
+
+  getGoals(){
+    return Goal;
+  }
+  constructor() { }
+}
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service'
 
 @Component({
   selector: 'app-goal',
@@ -8,15 +10,19 @@ import { Goal } from '../goal';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+  
+  goals: Goal[];
+  alertService:AlertService;
 
-  goals: Goal[] = [
+  
+  /* = [
     new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
     new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
     new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
     new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
     new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
     new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
-  ];
+  ];*/
 
   addNewGoal(goal){
     let goalLength = this.goals.length;
@@ -35,11 +41,15 @@ export class GoalComponent implements OnInit {
 
       if (toDelete){
         this.goals.splice(index,1)
+        this.alertService.alertMe("The goal has been deleted.");
       }
     }
   }
 
-  constructor() { }
+  constructor(goalService:GoalService, alertService:AlertService) {
+    this.goals = goalService.getGoals();
+    this.alertService = alertService;
+  }
 
   ngOnInit() {
   }
